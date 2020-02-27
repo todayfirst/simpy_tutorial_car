b9cd3d7704b67e277feba5c0944b6a474ec9d2b1
todayfirst <todayfirst@snu.ac.kr>
2020-02-27 16:42:32 +0900

    2nd

--- car.py
+++ car.py
@@ -100,3 +100,23 @@ env = simpy.Environment()
 car = Car(env)
 env.process(driver(env, car))
 env.run(15)
+
+
+####
+def car(env, name, bcs, driving_time, charge_time):
+    yield env.timeout(driving_time)
+
+    print('%s arriving at %d' % (name, env.now))
+    with bcs.request() as req:
+        yield req
+
+        print('%s starting to charge at %s' % (name, env.now))
+        yield env.timeout(charge_time)
+        print('%s leaving the bcs at %s' % (name, env.now))
+
+env = simpy.Environment()
+bcs = simpy.Resource(env, capacity=2)
+for i in range(4):
+    env.process(car(env, 'Car %d' % i, bcs, i*2, 5))
+
+env.run(until = 20)
